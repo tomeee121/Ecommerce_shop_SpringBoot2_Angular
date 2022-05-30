311bf43e71dbc1eafae3a28567966aa9fd006440
Tomasz Borowski <bortom112233@gmail.com>
2022-05-30 17:46:27 +0200

    Added entities, changesets and repos for state, country

---
+++ src/main/java/pl/project/wwsis/ecommerceshop/DAO/StateRepo.java
@@ -0,0 +1,17 @@
+package pl.project.wwsis.ecommerceshop.DAO;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
+import pl.project.wwsis.ecommerceshop.model.State;
+
+import java.util.List;
+
+@Repository
+@CrossOrigin("http://localhost:4200")
+@RepositoryRestResource
+public interface StateRepo extends JpaRepository<State, Integer> {
+    List<State> findStatesByCountryCode(@RequestParam(name = "code") String code);
+}
---
+++ src/main/java/pl/project/wwsis/ecommerceshop/model/State.java
@@ -0,0 +1,49 @@
+package pl.project.wwsis.ecommerceshop.model;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class State {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "country_id")
+    private Country country;
+
+
+    public State() {
+    }
+
+    public State(String name, Country country) {
+        this.name = name;
+        this.country = country;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
+}
