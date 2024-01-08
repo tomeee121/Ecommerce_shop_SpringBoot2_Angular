2d78a8433f017c6fddbb5ebf781b07020c104941
Tomasz Borowski <bortom112233@gmail.com>
2024-01-08 19:38:33 +0100

    * add 8080 port as host port for EC2's backend (now frontend is on 80 host port)

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
-    apiCustomerUrl: 'http://shop-ecommerce.eba-6j242pkh.eu-north-1.elasticbeanstalk.com',
-    shoppingHistoryUrlBase: 'http://shop-ecommerce.eba-6j242pkh.eu-north-1.elasticbeanstalk.com/customer/shopping-history',
-    allShoppingHistoryUrlBase:'http://shop-ecommerce.eba-6j242pkh.eu-north-1.elasticbeanstalk.com/customer/all-shopping-history',
-    deleteOrderUrlBase: 'http://shop-ecommerce.eba-6j242pkh.eu-north-1.elasticbeanstalk.com/customer/deleteOrder',
-    customerCustomUrl: 'http://shop-ecommerce.eba-6j242pkh.eu-north-1.elasticbeanstalk.com/customer',
-    baseStateUrl: 'http://shop-ecommerce.eba-6j242pkh.eu-north-1.elasticbeanstalk.com/api/states',
-    baseCountryUrl: 'http://shop-ecommerce.eba-6j242pkh.eu-north-1.elasticbeanstalk.com/api/countries',
-    baseUrlProducts: 'http://shop-ecommerce.eba-6j242pkh.eu-north-1.elasticbeanstalk.com/api/products',
-    categoryUrl: 'http://shop-ecommerce.eba-6j242pkh.eu-north-1.elasticbeanstalk.com/api/product-category',
-    purchaseUrl: 'http://shop-ecommerce.eba-6j242pkh.eu-north-1.elasticbeanstalk.com/checkout/purchase'
+    apiCustomerUrl: 'http://shop-ecommerce.eba-6j242pkh.eu-north-1.elasticbeanstalk.com:8080',
+    shoppingHistoryUrlBase: 'http://shop-ecommerce.eba-6j242pkh.eu-north-1.elasticbeanstalk.com:8080/customer/shopping-history',
+    allShoppingHistoryUrlBase:'http://shop-ecommerce.eba-6j242pkh.eu-north-1.elasticbeanstalk.com:8080/customer/all-shopping-history',
+    deleteOrderUrlBase: 'http://shop-ecommerce.eba-6j242pkh.eu-north-1.elasticbeanstalk.com:8080/customer/deleteOrder',
+    customerCustomUrl: 'http://shop-ecommerce.eba-6j242pkh.eu-north-1.elasticbeanstalk.com:8080/customer',
+    baseStateUrl: 'http://shop-ecommerce.eba-6j242pkh.eu-north-1.elasticbeanstalk.com:8080/api/states',
+    baseCountryUrl: 'http://shop-ecommerce.eba-6j242pkh.eu-north-1.elasticbeanstalk.com:8080/api/countries',
+    baseUrlProducts: 'http://shop-ecommerce.eba-6j242pkh.eu-north-1.elasticbeanstalk.com:8080/api/products',
+    categoryUrl: 'http://shop-ecommerce.eba-6j242pkh.eu-north-1.elasticbeanstalk.com:8080/api/product-category',
+    purchaseUrl: 'http://shop-ecommerce.eba-6j242pkh.eu-north-1.elasticbeanstalk.com:8080/checkout/purchase'
 };
